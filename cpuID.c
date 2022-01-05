9f6950e7ed22096f5f4e3f347a094745e8a99652
saarmichael <saarmichael@gmail.com>
2022-01-06 01:41:14 +0200

    cpuID.c

---
+++ cpuID.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a[4];
+    __asm__("mov $0x1 , %eax\n\t");
+    __asm__("cpuid\n\t");
+    __asm__("mov %%eax, %0\n\t":"=r" (a[0])); //gives model and family
+    __asm__("mov %%ebx, %0\n\t":"=r" (a[1])); //gives additional feature info
+    __asm__("mov %%ecx, %0\n\t":"=r" (a[2])); //feature flags
+    __asm__("mov %%edx, %0\n\t":"=r" (a[3])); //feature flags
+    printf("ecx: %d\n", a[2]);
+    printf("edx: %d\n", a[3]);
+    return 0;
+}
