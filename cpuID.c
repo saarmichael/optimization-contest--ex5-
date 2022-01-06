e6f7eb65f40eb31b6a6889ca02dda55e153f1811
saarmichael <saarmichael@gmail.com>
2022-01-06 23:28:20 +0200

    research

--- cpuID.c
+++ cpuID.c
@@ -10,5 +10,13 @@ int main() {
     __asm__("mov %%edx, %0\n\t":"=r" (a[3])); //feature flags
     printf("ecx: %d\n", a[2]);
     printf("edx: %d\n", a[3]);
+    
+    int b[3];
+__asm__("mov $0x0, %eax\n\t");
+__asm__("cpuid\n\t");
+__asm__("mov %%ebx, %0\n\t":"=r" (b[0]));
+__asm__("mov %%edx, %0\n\t":"=r" (b[1]));
+__asm__("mov %%ecx, %0\n\t":"=r" (b[2]));
+printf ("%s\n", &b);
     return 0;
 }
