642b8e0ad5d35fdc16985b70b5b6c2ee25fc7d7f
saarmichael <saarmichael@gmail.com>
2022-01-11 00:22:04 +0200

    small opts

--- myfunction.c
+++ myfunction.c
@@ -236,6 +236,225 @@ void sharp(int pdim, unsigned char* src, char* dst) {
 	int dim = pdim * 3;
 	// copy the first row of pixels to dst
 	memcpy(dst, src, dim);
+	// compy the last row of pixels to dst
+	memcpy(dst + ((pdim - 1)*dim), src + ((pdim - 1)*dim), dim);
+	unsigned char* ar = src;
+	unsigned char* ag = src + 1;
+	unsigned char* ab = src + 2;
+	unsigned char* br = src + dim;
+	unsigned char* bg = src + dim + 1;
+	unsigned char* bb = src + dim + 2;
+	unsigned char* cr = src + dim + dim;
+	unsigned char* cg = src + dim + dim + 1;
+	unsigned char* cb = src + dim + dim + 2;
+	unsigned char* xr = src + 3;
+	unsigned char* xg = src + 4;
+	unsigned char* xb = src + 5;
+	unsigned char* yr = src + dim + 3;
+	unsigned char* yg = src + dim + 4;
+	unsigned char* yb = src + dim + 5;
+	unsigned char* zr = src + dim + dim + 3;
+	unsigned char* zg = src + dim + dim + 4;
+	unsigned char* zb = src + dim + dim + 5;
+	
+	unsigned char* ur = src + 6;
+	unsigned char* ug = src + 7;
+	unsigned char* ub = src + 8;
+	unsigned char* vr = src + dim + 6;
+	unsigned char* vg = src + dim + 7;
+	unsigned char* vb = src + dim + 8;
+	unsigned char* wr = src + dim + dim + 6;
+	unsigned char* wg = src + dim + dim + 7;
+	unsigned char* wb = src + dim + dim + 8;
+	char* destr = dst + dim + 3;
+	char* destg = dst + dim + 4;
+	char* destb = dst + dim + 5;
+	
+	register int redSum, greenSum, blueSum;
+	unsigned int i, j;
+	for (i = 1; i < pdim - 1; i++) {
+		destr = dst + dim*i + 3*sizeof(unsigned char);
+		destg = dst + dim*i + 4*sizeof(unsigned char);
+		destb = dst + dim*i + 5*sizeof(unsigned char);
+		ar = src + dim*(i-1);
+		ag = src + dim*(i-1) + 1*sizeof(unsigned char);
+		ab = src + dim*(i-1) + 2*sizeof(unsigned char);
+		br = src + dim*(i);
+		bg = src + dim*(i) + 1*sizeof(unsigned char);
+		bb = src + dim*(i) + 2*sizeof(unsigned char);
+		cr = src + dim*(i+1);
+		cg = src + dim*(i+1) + 1*sizeof(unsigned char);
+		cb = src + dim*(i+1) + 2*sizeof(unsigned char);
+		xr = src + dim*(i-1) + 3*sizeof(unsigned char);
+		xg = src + dim*(i-1) + 4*sizeof(unsigned char);
+		xb = src + dim*(i-1) + 5*sizeof(unsigned char);
+		yr = src + dim*(i) + 3*sizeof(unsigned char);
+		yg = src + dim*(i) + 4*sizeof(unsigned char);
+		yb = src + dim*(i) + 5*sizeof(unsigned char);
+		zr = src + dim*(i+1) + 3*sizeof(unsigned char);
+		zg = src + dim*(i+1) + 4*sizeof(unsigned char);
+		zb = src + dim*(i+1) + 5*sizeof(unsigned char);
+		ur = src + dim*(i-1) + 6*sizeof(unsigned char);
+		ug = src + dim*(i-1) + 7*sizeof(unsigned char);
+		ub = src + dim*(i-1) + 8*sizeof(unsigned char);
+		vr = src + dim*(i) + 6*sizeof(unsigned char);
+		vg = src + dim*(i) + 7*sizeof(unsigned char);
+		vb = src + dim*(i) + 8*sizeof(unsigned char);
+		wr = src + dim*(i+1) + 6*sizeof(unsigned char);
+		wg = src + dim*(i+1) + 7*sizeof(unsigned char);
+		wb = src + dim*(i+1) + 8*sizeof(unsigned char);
+		// copy the first pixel to dst
+		*(destr - 3) = *br;
+		*(destg - 3) = *bg;
+		*(destb - 3) = *bb;
+		for ( j = 1; j < pdim - 1; j++) {
+			redSum = 0;
+			greenSum = 0;
+			blueSum = 0;
+			// the equivalent of applykernel multiplication
+			redSum   -= (int)*ar + (int)*xr + (int)*ur;
+			greenSum -= (int)*ag + (int)*xg + (int)*ug;
+			blueSum  -= (int)*ab + (int)*xb + (int)*ub;
+			redSum   -= (int)*br - 9*(int)*yr + (int)*vr;
+			greenSum -= (int)*bg - 9*(int)*yg + (int)*vg;
+			blueSum  -= (int)*bb - 9*(int)*yb + (int)*vb;
+			redSum   -= (int)*cr + (int)*zr + (int)*wr;
+			greenSum -= (int)*cg + (int)*zg + (int)*wg;
+			blueSum  -= (int)*cb + (int)*zb + (int)*wb;
+			
+			// put the values in the target 'pixel'
+			*destr = (unsigned char)(MIN(MAX(redSum, 0), 255));
+			*destg = (unsigned char)(MIN(MAX(greenSum, 0), 255));
+			*destb = (unsigned char)(MIN(MAX(blueSum, 0), 255));
+			// moving on to the next 'pixel'
+			destr += 3*sizeof(unsigned char);
+			destg += 3*sizeof(unsigned char);
+			destb += 3*sizeof(unsigned char);
+			ar += 3*sizeof(unsigned char);
+			ag += 3*sizeof(unsigned char);
+			ab += 3*sizeof(unsigned char);
+			br += 3*sizeof(unsigned char);
+			bg += 3*sizeof(unsigned char);
+			bb += 3*sizeof(unsigned char);
+			cr += 3*sizeof(unsigned char);
+			cg += 3*sizeof(unsigned char);
+			cb += 3*sizeof(unsigned char);
+			xr += 3*sizeof(unsigned char);
+			xg += 3*sizeof(unsigned char);
+			xb += 3*sizeof(unsigned char);
+			yr += 3*sizeof(unsigned char);
+			yg += 3*sizeof(unsigned char);
+			yb += 3*sizeof(unsigned char);
+			zr += 3*sizeof(unsigned char);
+			zg += 3*sizeof(unsigned char);
+			zb += 3*sizeof(unsigned char);
+			ur += 3*sizeof(unsigned char);
+			ug += 3*sizeof(unsigned char);
+			ub += 3*sizeof(unsigned char);
+			vr += 3*sizeof(unsigned char);
+			vg += 3*sizeof(unsigned char);
+			vb += 3*sizeof(unsigned char);
+			wr += 3*sizeof(unsigned char);
+			wg += 3*sizeof(unsigned char);
+			wb += 3*sizeof(unsigned char);
+		}
+		// copy the last pixel to dst
+		*(destr) = *(vr - 3);
+		*(destg) = *(vg - 3);
+		*(destb) = *(vb - 3);
+	}
+}
+
+
+void sharp_less_vars(int pdim, unsigned char* src, char* dst) {
+	int dim = pdim * 3;
+	// copy the first row of pixels to dst
+	memcpy(dst, src, dim);
+	// copy the last row of pixels to dst
+	memcpy(dst + ((pdim - 1)*dim), src + ((pdim - 1)*dim), dim);
+	register unsigned char* a = src;
+	register unsigned char* b = src + dim;
+	register unsigned char* c = src + dim + dim;
+	register unsigned char* x = src + 3;
+	register unsigned char* y = src + dim + 3;
+	register unsigned char* z = src + dim + dim + 3;
+	register unsigned char* u = src + 6;
+	register unsigned char* v = src + dim + 6;
+	register unsigned char* w = src + dim + dim + 6;
+	char* destr = dst + dim + 3;
+	char* destg = dst + dim + 4;
+	char* destb = dst + dim + 5;
+	register int redSum, greenSum, blueSum;
+	unsigned int i, j;
+	for (i = 1; i < pdim - 1; i++) {
+
+		destr = dst + dim*i + 3*sizeof(unsigned char);
+		destg = dst + dim*i + 4*sizeof(unsigned char);
+		destb = dst + dim*i + 5*sizeof(unsigned char);
+
+		a = src + dim*(i-1);
+		b = src + dim*(i);
+		c = src + dim*(i+1);
+		x = src + dim*(i-1) + 3*sizeof(unsigned char);
+		y = src + dim*(i) + 3*sizeof(unsigned char);
+		z = src + dim*(i+1) + 3*sizeof(unsigned char);
+		u = src + dim*(i-1) + 6*sizeof(unsigned char);
+		v = src + dim*(i) + 6*sizeof(unsigned char);
+		w = src + dim*(i+1) + 6*sizeof(unsigned char);
+		// copy the first pixel to dst
+		*(destr - 3) = *b;
+		*(destg - 3) = *(b + 1);
+		*(destb - 3) = *(b + 2);
+		
+		for ( j = 1; j < pdim - 1; j++) {
+			redSum = 0;
+			greenSum = 0;
+			blueSum = 0;
+
+			redSum -=   (int)*(a) + (int)*(x) + (int)*(u) + (int)*(b) 
+			          + (int)*(v) + (int)*(c) + (int)*(z) + (int)*(w);
+			
+			redSum += 9 * (int)*(y);
+			a++; x++; u++;
+			b++; y++; v++;
+			c++; z++; w++;
+
+			greenSum -=   (int)*(a) + (int)*(x) + (int)*(u) + (int)*(b) 
+			            + (int)*(v) + (int)*(c) + (int)*(z) + (int)*(w);
+			
+			greenSum += 9 * (int)*(y);
+			a++; x++; u++;
+			b++; y++; v++;
+			c++; z++; w++;
+
+			blueSum -=   (int)*(a) + (int)*(x) + (int)*(u) + (int)*(b) 
+			           + (int)*(v) + (int)*(c) + (int)*(z) + (int)*(w);
+			
+			blueSum += 9 * (int)*(y);
+			a++; x++; u++;
+			b++; y++; v++;
+			c++; z++; w++;
+		
+			// put the values in the target 'pixel'
+			*destr = (unsigned char)(MIN(MAX(redSum, 0), 255));
+			*destg = (unsigned char)(MIN(MAX(greenSum, 0), 255));
+			*destb = (unsigned char)(MIN(MAX(blueSum, 0), 255));
+			// moving on to the next 'pixel'
+			destr += 3*sizeof(unsigned char);
+			destg += 3*sizeof(unsigned char);
+			destb += 3*sizeof(unsigned char);
+		}
+		// copy the last pixel to dst
+		*(destr) = *(v - 3);
+		*(destg) = *(v - 2);
+		*(destb) = *(v - 1);
+	}
+}
+
+void sharp_unrolled(int pdim, unsigned char* src, char* dst) {
+	int dim = pdim * 3;
+	// copy the first row of pixels to dst
+	memcpy(dst, src, dim);
 	// copy the last row of pixels to dst
 	memcpy(dst + ((pdim - 1)*dim), src + ((pdim - 1)*dim), dim);
 	register unsigned char* a = src;
@@ -786,6 +1005,223 @@ void filterChars(int pdim, unsigned char *src, char *dst) {
 	}
 }
 
+void filterChars_less_vars(int pdim, unsigned char *src, char *dst) {
+	int dim = pdim * 3;
+	// copy the first row of pixels to dst
+	memcpy(dst, src, dim);
+	// copy the last row of pixels to dst
+	memcpy(dst + ((pdim - 1)*dim), src + ((pdim - 1)*dim), dim);
+	register unsigned char* a = src;
+	register unsigned char* b = src + dim;
+	register unsigned char* c = src + dim + dim;
+	register unsigned char* x = src + 3;
+	register unsigned char* y = src + dim + 3;
+	register unsigned char* z = src + dim + dim + 3;
+	register unsigned char* u = src + 6;
+	register unsigned char* v = src + dim + 6;
+	register unsigned char* w = src + dim + dim + 6;
+	char* destr = dst + dim + 3;
+	char* destg = dst + dim + 4;
+	char* destb = dst + dim + 5;
+	register int redSum, greenSum, blueSum;
+	unsigned char *maxPixel, *minPixel;
+	register int sumL, sumM, sumR, min, max;
+	unsigned int i, j;
+	for (i = 1; i < pdim - 1; i++) {
+
+		destr = dst + dim*i + 3*sizeof(unsigned char);
+		destg = dst + dim*i + 4*sizeof(unsigned char);
+		destb = dst + dim*i + 5*sizeof(unsigned char);
+
+		// a = src + dim*(i-1);
+		// b = src + dim*(i);
+		// c = src + dim*(i+1);
+		// x = src + dim*(i-1) + 3*sizeof(unsigned char);
+		// y = src + dim*(i) + 3*sizeof(unsigned char);
+		// z = src + dim*(i+1) + 3*sizeof(unsigned char);
+		// u = src + dim*(i-1) + 6*sizeof(unsigned char);
+		// v = src + dim*(i) + 6*sizeof(unsigned char);
+		// w = src + dim*(i+1) + 6*sizeof(unsigned char);
+
+		// copy the first pixel to dst
+		*(destr - 3) = *b;
+		*(destg - 3) = *(b + 1);
+		*(destb - 3) = *(b + 2);
+
+		for ( j = 1; j < pdim - 1; j++) {
+			redSum = 0;
+			greenSum = 0;
+			blueSum = 0;
+			min = 766;
+			max = 0;
+
+			// the equivalent of applykernel multiplication
+			redSum   += (int)*a + (int)*x + (int)*u;
+			// calculate intensity
+			sumL = (int)*a;
+			sumM = (int)*x;
+			sumR = (int)*u;
+			// move to green
+			a++; x++; u++;
+			greenSum   += (int)*a + (int)*x + (int)*u;
+			// calculate intensity
+			sumL += (int)*a;
+			sumM += (int)*x;
+			sumR += (int)*u;
+			// move to blue
+			a++; x++; u++;
+			blueSum   += (int)*a + (int)*x + (int)*u;
+			// calculate intensity
+			sumL += (int)*a;
+			sumM += (int)*x;
+			sumR += (int)*u;
+			a++; x++; u++;
+			
+			if (sumL <= min) {
+				min = sumL;
+				minPixel = a - 3; 
+			}
+			if (sumL > max) {
+				max = sumL;
+				maxPixel = a - 3;
+			}
+			if (sumM <= min) {
+				min = sumM;
+				minPixel = a; // x - 3
+			}
+			if (sumM > max) {
+				max = sumM;
+				maxPixel = a; // x - 3
+			}
+			if (sumR <= min) {
+				min = sumR;
+				minPixel = x; // u - 3
+			}
+			if (sumR > max) {
+				max = sumR;
+				maxPixel = x; // u - 3
+			}
+			
+
+			redSum   += (int)*b + (int)*y + (int)*v;
+			// calculate intensity
+			sumL = (int)*b;
+			sumM = (int)*y;
+			sumR = (int)*v;
+			// move to green
+			b++; y++; v++;
+			greenSum   += (int)*b + (int)*y + (int)*v;
+			// calculate intensity
+			sumL += (int)*b;
+			sumM += (int)*y;
+			sumR += (int)*v;
+			// move to blue
+			b++; y++; v++;
+			blueSum   += (int)*b + (int)*y + (int)*v;
+			// calculate intensity
+			sumL += (int)*b;
+			sumM += (int)*y;
+			sumR += (int)*v;
+			b++; y++; v++;
+			
+			if (sumL <= min) {
+				min = sumL;
+				minPixel = b - 3; 
+			}
+			if (sumL > max) {
+				max = sumL;
+				maxPixel = b - 3;
+			}
+			if (sumM <= min) {
+				min = sumM;
+				minPixel = b; // y - 3
+			}
+			if (sumM > max) {
+				max = sumM;
+				maxPixel = b; // y - 3
+			}if (sumR <= min) {
+				min = sumR;
+				minPixel = y; // v - 3
+			}
+			if (sumR > max) {
+				max = sumR;
+				maxPixel = y; // v - 3
+			}
+			
+			
+			redSum  += (int)*c + (int)*z + (int)*w;
+			// calculate intensity
+			sumL = (int)*c;
+			sumM = (int)*z;
+			sumR = (int)*w;
+			// move to green
+			c++; z++; w++;
+			greenSum  += (int)*c + (int)*z + (int)*w;
+			// calculate intensity
+			sumL += (int)*c;
+			sumM += (int)*z;
+			sumR += (int)*w;
+			// move to blue
+			c++; z++; w++;
+			blueSum  += (int)*c + (int)*z + (int)*w;
+			// calculate intensity
+			sumL += (int)*c;
+			sumM += (int)*z;
+			sumR += (int)*w;
+			c++; z++; w++;
+			
+			if (sumL <= min) {
+				min = sumL;
+				minPixel = c - 3; 
+			}
+			if (sumL > max) {
+				max = sumL;
+				maxPixel = c - 3;
+			}if (sumM <= min) {
+				min = sumM;
+				minPixel = c; // z - 3
+			}
+			if (sumM > max) {
+				max = sumM;
+				maxPixel = c; // z - 3
+			}if (sumR <= min) {
+				min = sumR;
+				minPixel =  z; // w - 3
+			}if (sumR > max) {
+				max = sumR;
+				maxPixel = z; // w - 3
+			}
+			
+			// subtract the value of max and min pixels
+			redSum   -= (int)*(maxPixel)   + (int)*(minPixel);
+			greenSum -= (int)*(maxPixel + 1)   + (int)*(minPixel + 1);
+			blueSum  -= (int)*(maxPixel + 2)     + (int)*(minPixel + 2);
+
+			// put the values in the target 'pixel'
+			redSum = (redSum / 7);
+			greenSum = (greenSum / 7);
+			blueSum = (blueSum / 7);
+			*destr = (unsigned char)(MIN(MAX(redSum, 0), 255));
+			*destg = (unsigned char)(MIN(MAX(greenSum, 0), 255));
+			*destb = (unsigned char)(MIN(MAX(blueSum, 0), 255));
+
+			// moving on to the next 'pixel'
+			destr += 3*sizeof(unsigned char);
+			destg += 3*sizeof(unsigned char);
+			destb += 3*sizeof(unsigned char);
+		}
+		// copy the last pixel to dst
+		*(destr) = *(y);
+		*(destg) = *(y + 1);
+		*(destb) = *(y + 2);
+
+		a+=6; x+=6; u+=6;
+		b+=6; y+=6; v+=6;
+		c+=6; z+=6; w+=6;
+	}
+ 
+}
+
 
 void doConvolution1(Image *image) {
 
@@ -799,7 +1235,7 @@ void doConvolution2(Image *image) {
 
 	char* newImage = (unsigned char*)malloc(3*m*n*sizeof(unsigned char));
 	
-	sharp(m, image->data, newImage);
+	sharp_less_vars(m, image->data, newImage);
 	image->data = newImage;
 }
 
@@ -807,7 +1243,7 @@ void doConvolution3(Image *image) {
 
 	char* newImage = (unsigned char*)malloc(3*m*n*sizeof(unsigned char));
 	
-	filterChars(m, image->data, newImage);
+	filterChars_less_vars(m, image->data, newImage);
 	image->data = newImage;
 }
 
